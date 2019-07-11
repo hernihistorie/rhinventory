e8d0a67c926cce73f190b2d88b1c58286a4a5bac
BlackChar <blackchar@pikachu.cz>
2019-07-11 18:55:37 +0200

    added old RPPCA database structure (roughly)

--- db.py
+++ db.py
@@ -21,6 +21,50 @@ class Asset(db.Model):
     name = db.Column(db.String(255))
 
 
+class AuditLog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    item_table = db.Column(db.String(255))
+    item_id = db.Column(db.Integer)
+    action = db.Column(db.String(255))
+
+
+class Benchmark(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    benchmark_run_id = db.Column(db.Integer, db.ForeignKey('benchmark_runs.id'))
+    benchmark_type_id = db.Column(db.Integer, db.ForeignKey('benchmark_type.id'))
+    computer_id = db.Column(db.Integer, db.ForeignKey('computer.id'))
+    rundate = db.Column(db.Integer)  # TODO: transform to DateTime
+    runby = db.Column(db.Integer)
+    runok = db.Column(db.Integer)
+    simpleresult = db.Column(db.Numeric)
+
+
+class BenchmarkResult(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    benchmark_id = db.Column(db.Integer, db.ForeignKey('benchmark.id'))
+    mime = db.Column(db.String(255))
+    content = db.Column(db.LargeBinary)
+    description = db.Column(db.String(4096))
+    note = db.Column(db.String(4096))
+
+
+class BenchmarkRun(db.Model):
+    __tablename__ = 'benchmark_runs'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    rundate = db.Column(db.DateTime)
+    runby = db.Column(db.Integer)  # TODO: link to user
+
+
+class BenchmarkType(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    description = db.Column(db.String(4096))
+    inputhelp = db.Column(db.String(4096))
+    resulthelp = db.Column(db.String(4096))
+
+
 class Computer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
@@ -30,7 +74,7 @@ class Computer(db.Model):
     description = db.Column(db.String(255))
     serial = db.Column(db.String(255))
     image = db.Column(db.String(255))
-    status = db.Column(db.Integer)
+    status = db.Column(db.Integer, db.ForeignKey('status.id'))
     condition = db.Column(db.String(255))
     acq_from = db.Column(db.String(255))
     acq_by = db.Column(db.String(255))
@@ -38,9 +82,43 @@ class Computer(db.Model):
     note = db.Column(db.String())
 
 
+class Hardware(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    hardware_type = db.Column(db.Integer, db.ForeignKey('hardware_type.id'))
+    computer_id = db.Column(db.Integer, db.ForeignKey('computer.id'))
+    name = db.Column(db.String(255))
+    manufacturer = db.Column(db.String(255))
+    model = db.Column(db.String(255))
+    serial = db.Column(db.String(255))
+    description = db.Column(db.String(255))
+    image = db.Column(db.String(255))
+    status = db.Column(db.Integer, db.ForeignKey('status.id'))
+    condition = db.Column(db.String(255))
+    acq_from = db.Column(db.String(255))
+    acq_by = db.Column(db.String(255))
+    acq_for = db.Column(db.String(255))
+    note = db.Column(db.String())
+
+
+class HardwareType(db.Model):
+    __tablename__ = 'hardware_type'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    description = db.Column(db.String(255))
+    icon = db.Column(db.String(255))
+
+    def __str__(self):
+        return self.name
+
+
+class Status(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+
+
 admin = Admin(app)
 
-for table in (Asset, Computer):
+for table in (Asset, Benchmark, BenchmarkType, Computer, Hardware):
     admin.add_view(ModelView(table, db.session))
 
 if __name__ == '__main__':
