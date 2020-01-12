057eb6726bafc7d3cd1708c6ecb4f5e8a165ebd0
Sanqui <me@sanqui.net>
2020-01-12 21:03:07 +0100

    Implement label PNG generation from DB

--- rhinventory/app.py
+++ rhinventory/app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, flash, redirect, url_for, send_file
+from flask import Flask, render_template, flash, redirect, url_for, send_file, Response
 from jinja2 import StrictUndefined
 
 from rhinventory.extensions import db, admin, debug_toolbar
 from rhinventory.admin import add_admin_views
+from rhinventory.db import Asset
 
 def create_app(config_object='rhinventory.config'):
     app = Flask(__name__.split('.')[0])
@@ -26,6 +27,18 @@ def create_app(config_object='rhinventory.config'):
                         #as_attachment=True,
                         #attachment_filename='a_file.txt',
                         mimetype='image/svg+xml')
+    
+    @app.route('/label/<int:asset_id>')
+    def label_endpoint(asset_id):
+        from rhinventory.labels import make_label
+
+        asset = Asset.query.get(asset_id)
+
+        id = f"RH{asset_id:05}"
+        label_filename = make_label(id, asset.custom_code, asset.name)
+
+        #return Response(label, mimetype='image/svg+xml')
+        return send_file(open(label_filename, 'rb'), mimetype='image/png')
     
     return app
 
--- rhinventory/labels.py
+++ rhinventory/labels.py
@@ -1,7 +1,17 @@
+from os import system
 from io import BytesIO
 
+from bs4 import BeautifulSoup as BS
 import barcode
 from barcode.writer import ImageWriter
+
+import jinja2
+
+def render_jinja_html(template_loc, file_name, **context):
+
+    return jinja2.Environment(
+        loader=jinja2.FileSystemLoader(template_loc+'/')
+    ).get_template(file_name).render(context)
 
 def make_barcode(text):
     Code128 = barcode.get_barcode_class('code128')
@@ -12,4 +22,23 @@ def make_barcode(text):
     fp.seek(0)
 
     return fp
+
+def make_label(id, custom_code, name):
+    PNG_WIDTH = 696
+    
+    fp = make_barcode(id)
+    soup = BS(fp, 'lxml')
+    rects = soup.find(id="barcode_group")
+
+    label_svg = render_jinja_html('rhinventory/labels', 'label.svg',
+        name=name, custom_code=custom_code, id=id, 
+        barcode_rects=rects,
+        rh_logo_href="../rh_logo_greyscale.png")
+    
+    filename = f'rhinventory/labels/out/{id}'
+    open(filename+'.svg', 'w').write(label_svg)
+
+    system(f'inkscape -z {filename}.svg -e {filename}.png -w {PNG_WIDTH}')
+
+    return filename+'.png'
 
