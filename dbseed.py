fe207bd08ab4490770ca2108cc77838406cc2005
BlackChar <blackcharcz@gmail.com>
2020-01-13 00:06:23 +0100

    dbseed: reformat, add metadata mapping

--- dbseed.py
+++ dbseed.py
@@ -1,80 +1,124 @@
-from sys import argv
+#from sys import argv
 import csv
 from datetime import datetime
 from decimal import Decimal
 from rhinventory.app import create_app
 from rhinventory.extensions import db
 from rhinventory.db import Asset, AssetStatus, Category
-import json
+#import json
 from pprint import pprint
 
 
 csv_file = {
-    'game':'./docs/GM.csv',
-    'console':'./docs/GC.csv',
-    'console accesory':'./docs/PP.csv',
-    'other software':'./docs/SW.csv',
-    'keyboard':'./docs/Kláv.csv',
-    'computer mouse':'./docs/Myš.csv',
-    'television':'./docs/TV.csv',
-    'monitor':'./docs/M.csv',
+    'game': './docs/GM.csv',
+    'console': './docs/GC.csv',
+    'console accesory': './docs/PP.csv',
+    'other software': './docs/SW.csv',
+    'keyboard': './docs/Kláv.csv',
+    'computer mouse': './docs/Myš.csv',
+    'television': './docs/TV.csv',
+    'monitor': './docs/M.csv',
 
 }
 
-asset_titles={
+asset_titles = {
     "keyboard": "klávesnice",
     "computer mouse": "Myš",
     "television": "Televizor",
-    "monitor": "Monitor",                    
-    }
+    "monitor": "Monitor",
+}
+
+
+game_meta = {
+    "Platform": "Platforma",
+    "Medium": "Formát",
+    "Product No": "Písmena a čísla",
+}
+
+console_meta = {
+}
+
+mouse_meta = {
+    "Color": "Vzhled",
+    "Connection": "Typ",
+    "Type": "Styl",
+
+}
+
+keyboard_meta = {
+    "Color": "Vzhled",
+    "Connection": "Typ",
+}
+
+television_meta = {
+    "Size": "Úhlopříčka",
+
+}
+
+monitor_meta = {
+    "Size": "Úhlopříčka",
+}
+
 
 def row_is_valid(row):
     if "Název" in row.keys():
         if row["Název"].strip() != '' and row["Název"].strip() != '-' and ('reserved' not in row["Název"].strip().lower()):
             return True
-        else: return False
+        else:
+            return False
     elif "Typ" in row.keys():
-        if row["Typ"].strip() != '': return True
-        else: return False
+        if row["Typ"].strip() != '':
+            return True
+        else:
+            return False
     elif "Výrobce" in row.keys():
-        if row["Výrobce"].strip() != '': return True
-        else: return False
-    else: return False
+        if row["Výrobce"].strip() != '':
+            return True
+        else:
+            return False
+    else:
+        return False
 
-app=create_app()
-with app.app_context(): 
-#    db.create_all()
+
+app = create_app()
+with app.app_context():
+    #    db.create_all()
 
     for key in csv_file:
-        with open (csv_file[key], newline='') as f:
+        with open(csv_file[key], newline='') as f:
             reader = csv.reader(f)
-            cat=Category.query.filter(Category.name==key).one()
-            pprint ([key, csv_file[key], cat.prefix])
+            cat = Category.query.filter(Category.name == key).one()
+            pprint([key, csv_file[key], cat.prefix])
             header = next(reader)
-
-             
 
             for row in reader:
                 row = dict(zip(header, row))
                 if row_is_valid(row):
-                    #pprint(row)
-                    title=row.get("Název", asset_titles.get(key,"")).strip()
-                    
+                    # pprint(row)
+                    title = row.get("Název", asset_titles.get(key, "")).strip()
+
                     # pprint ([row["Inv. č."], title])
                     asset = Asset(
                         category=cat,
-                        name= title,
+                        name=title,
                         manufacturer=row["Výrobce"].strip(),
-                        note=row.get("Poznámka","").strip(),
-                        model=row.get("Model","").strip(),
-                        custom_code=int(row["Inv. č."].strip().replace(cat.prefix,"")),
+                        note=row.get("Poznámka", "").strip(),
+                        model=row.get("Model", "").strip(),
+                        # TODO Handle null
+                        serial=row.get("Sériové číslo", "").strip(),
+                        custom_code=int(
+                            row["Inv. č."].strip().replace(cat.prefix, "")),
                         num_photos=0,
                         condition=0,
                         functionality=0,
                         status=AssetStatus.unknown
                     )
-                    #pprint(asset)
+                    # pprint(asset)
+
+                 #    if cat.name == ""
+
                     db.session.add(asset)
+                    # TODO: log creation
 
             db.session.commit()
 
@@ -89,6 +133,3 @@ with app.app_context():
         #         log("Create", category)
         #     db_categories[code] = category
         #     return category
-
-
-
