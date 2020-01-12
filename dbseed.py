ba0adfd04244d4dede527b86a343ce2a68d59f98
BlackChar <blackcharcz@gmail.com>
2020-01-12 18:04:46 +0100

    csv import (broken)

--- dbseed.py
+++ dbseed.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from decimal import Decimal
 from rhinventory.app import create_app
 from rhinventory.extensions import db
-from rhinventory.db import Asset
+from rhinventory.db import Asset, Category
 import json
 from pprint import pprint
 
@@ -23,10 +23,15 @@ with app.app_context():
 
         for row in reader:
             row = dict(zip(header, row))
-            pprint(row)
             asset = Asset(
+                name=row["Název"],
+                manufacturer=row["Výrobce"],
+                note=row["Poznámka"],
+                custom_code=int(row["Inv. č."].replace("GM","")),
+                category=Category.query.filter(Category.prefix=='GM')
                 
             )
+            pprint(Asset)
 
     # def get_category(code):
         # if code in db_categories:
