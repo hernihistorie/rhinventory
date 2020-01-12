430dd9277a74d5a13139e9c3a47befc261076c7e
BlackChar <blackcharcz@gmail.com>
2020-01-12 22:08:01 +0100

    dbseed - finished importing assets

--- dbseed.py
+++ dbseed.py
@@ -11,9 +11,35 @@ from pprint import pprint
 
 csv_file = {
     'game':'/mnt/z/GM.csv',
-    'computer':'/mnt/z/PC.csv',
-    'console':'/mnt/z/GC.csv'
+    'console':'/mnt/z/GC.csv',
+    'console accesory':'/mnt/z/PP.csv',
+    'other software':'/mnt/z/SW.csv',
+    'keyboard':'/mnt/z/Kláv.csv',
+    'computer mouse':'/mnt/z/Myš.csv',
+    'television':'/mnt/z/TV.csv',
+    'monitor':'/mnt/z/M.csv',
+
 }
+
+asset_titles={
+    "keyboard": "klávesnice",
+    "computer mouse": "Myš",
+    "television": "Televizor",
+    "monitor": "Monitor",                    
+    }
+
+def row_is_valid(row):
+    if "Název" in row.keys():
+        if row["Název"].strip() != '' and row["Název"].strip() != '-' and ('reserved' not in row["Název"].strip().lower()):
+            return True
+        else: return False
+    elif "Typ" in row.keys():
+        if row["Typ"].strip() != '': return True
+        else: return False
+    elif "Výrobce" in row.keys():
+        if row["Výrobce"].strip() != '': return True
+        else: return False
+    else: return False
 
 app=create_app()
 with app.app_context(): 
@@ -24,16 +50,21 @@ with app.app_context():
             pprint ([key, csv_file[key], cat.prefix])
             header = next(reader)
 
+             
+
             for row in reader:
                 row = dict(zip(header, row))
-                if row["Název"].strip() != '' and row["Název"].strip() != '-' and ('reserved' not in row["Název"].strip().lower()) :
-                    pprint ([row["Název"], row["Inv. č."]])
+                if row_is_valid(row):
+                    #pprint(row)
+                    title=row.get("Název", asset_titles.get(key,"")).strip()
+                    
+                    pprint ([row["Inv. č."], title])
                     asset = Asset(
                         category=cat,
-                        name=row["Název"].strip(),
+                        name= title,
                         manufacturer=row["Výrobce"].strip(),
-                        note=row["Poznámka"].strip(),
-                        custom_code=int(row["Inv. č."].replace(cat.prefix,"")),
+                        note=row.get("Poznámka","").strip(),
+                        custom_code=int(row["Inv. č."].strip().replace(cat.prefix,"")),
                         condition=0,
                         functionality=0,
                         status=AssetStatus.unknown
