3a5c18ec84d391cb324b55d0370a9513db003d4d
Sanqui <me@sanqui.net>
2022-08-06 21:13:42 +0200

    Implement a number of real tests, particularly asset form

---
+++ tests/test_rhinventory.py
@@ -0,0 +1,107 @@
+import pytest
+
+from flask_login import login_user
+
+from rhinventory.app import create_app
+from rhinventory.extensions import db
+from rhinventory.db import User, Organization, Category
+from rhinventory.models.asset import Asset
+
+class TestAppConfig:
+    TESTING = True
+    DATABASE_URL = "sqlite:///:memory:"
+    GITHUB_CLIENT_ID = None
+    GITHUB_CLIENT_SECRET = None
+    FILES_DIR = "files"
+    SECRET_KEY = "abc"
+
+
+@pytest.fixture()
+def app():
+    app = create_app(config_object=TestAppConfig)
+
+    with app.app_context():
+        db.create_all()
+
+        user = User(username="pytest", read_access=True, write_access=True, admin=True)
+        db.session.add(user)
+
+        org = Organization(name="Testing z.s.")
+        db.session.add(org)
+
+        category = Category(name="game")
+        db.session.add(category)
+
+        db.session.commit()
+
+        user_id = user.id
+
+    @app.login_manager.request_loader
+    def load_user_from_request(request):
+        return User.query.get(user_id)
+    
+    yield app
+
+    del app
+
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture()
+def runner(app):
+    return app.test_cli_runner()
+
+
+def test_index(client):
+    response = client.get("/admin/")
+    assert response.status_code == 200
+    assert "Vítejte" in response.data.decode('utf-8')
+
+
+def test_asset_list(client):
+    response = client.get("/admin/asset/")
+    assert response.status_code == 200
+
+
+def test_asset_new(client):
+    url = "/admin/asset/new/"
+    asset_name = "Test Object 123"
+
+    client.get(url)
+    response = client.post(url, data={
+        "organization": "1",
+        "category": "1",
+        "name": asset_name,
+    })
+    assert response.status_code == 302
+
+    response = client.get("/admin/asset/")
+    assert response.status_code == 200
+    assert asset_name in response.data.decode('utf-8')
+
+    response = client.get("/admin/asset/details/?id=1")
+    assert response.status_code == 200
+    assert asset_name in response.data.decode('utf-8')
+
+    response = client.get("/admin/asset/edit/?id=1")
+    assert response.status_code == 200
+    assert asset_name in response.data.decode('utf-8')
+
+
+def test_transaction_list(client):
+    response = client.get("/admin/transaction/")
+    assert response.status_code == 200
+
+
+def test_file_list(client):
+    response = client.get("/admin/file/")
+    assert response.status_code == 200
+
+
+def test_file_upload(client):
+    response = client.get("/admin/file/upload/")
+    assert response.status_code == 200
+
