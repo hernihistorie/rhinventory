8434998e59b65acbc7bce93d9b25e43ce9547176
Sanqui <me@sanqui.net>
2022-10-26 22:02:17 +0200

    File upload form: Make auto_assign default to true again

--- rhinventory/forms.py
+++ rhinventory/forms.py
@@ -7,7 +7,7 @@ class FileForm(Form):
     files = MultipleFileField("Files")
     category = Select2Field("Category", choices=[(el.value, el.name) for el in FileCategory], coerce=int)
     batch_number = IntegerField("Batch number", render_kw={'readonly': True})
-    auto_assign = BooleanField("Auto assign")
+    auto_assign = BooleanField("Auto assign", render_kw={'checked':''})
 
 class FileAssignForm(Form):
     asset = Select2Field("asset", coerce=int)
