574ad4ab617aebd83752a58453efaca813652c20
Sanqui <me@sanqui.net>
2022-10-31 21:12:58 +0100

    alembic merge heads

---
+++ alembic/versions/990b9a3166b8_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 990b9a3166b8
+Revises: e937ede96e7d, a97d1f1210d8
+Create Date: 2022-10-31 21:12:41.306325
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '990b9a3166b8'
+down_revision = ('e937ede96e7d', 'a97d1f1210d8')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
