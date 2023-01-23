dfa706c612aa151b75dc7dd6df3cd0e42e9db597
Sanqui <me@sanqui.net>
2023-01-23 16:33:48 +0100

    alembic merge heads

---
+++ alembic/versions/46561199b0c1_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 46561199b0c1
+Revises: 6dc4b06bff6c, 4bbb9b42edc7
+Create Date: 2023-01-23 16:33:36.337483
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '46561199b0c1'
+down_revision = ('6dc4b06bff6c', '4bbb9b42edc7')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
