8547222dfdcb92489a18f498450d3b35e2242d87
Sanqui <me@sanqui.net>
2023-08-09 15:53:02 +0200

    Asset admin view: increase page size to 250

--- rhinventory/admin_views/asset.py
+++ rhinventory/admin_views/asset.py
@@ -164,6 +164,7 @@ class AssetView(CustomModelView):
 #        'condition': AssetCondition,
     }
     can_export = True
+    page_size = 250
 
     list_template = "admin/asset/list.html"
     details_template = "admin/asset/details.html"
