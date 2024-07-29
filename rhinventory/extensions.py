8c03f949c6b3fd8fef6412957d9e4f5de054c6e8
Sanqui <me@sanqui.net>
2024-07-29 17:27:05 +0200

    Remove Admin from page titles

--- rhinventory/extensions.py
+++ rhinventory/extensions.py
@@ -12,7 +12,7 @@ db.Model.asdict = lambda self: {c.name: getattr(self, c.name) for c in self.__ta
 # TODO remove this to upgrade to sqlalchemy 2.0.0
 db.Model.__allow_unmapped__ = True
 
-admin = Admin()
+admin = Admin(name="Sbírka Herní Archiv")
 debug_toolbar = DebugToolbarExtension()
 github = GitHub()
 login_manager = LoginManager()
