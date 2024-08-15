94c34a799e40b2d037f4f826d1eba2ea928813d5
Sanqui <me@sanqui.net>
2024-08-15 10:40:00 +0200

    Sentry: disable performance measurements until we get a new server

--- rhinventory/app.py
+++ rhinventory/app.py
@@ -34,11 +34,11 @@ def create_app(config_object='rhinventory.config'):
             config_object.SENTRY_DSN,
             # Set traces_sample_rate to 1.0 to capture 100%
             # of transactions for performance monitoring.
-            traces_sample_rate=1.0,
+            traces_sample_rate=0.0,
             # Set profiles_sample_rate to 1.0 to profile 100%
             # of sampled transactions.
             # We recommend adjusting this value in production.
-            profiles_sample_rate=1.0,
+            profiles_sample_rate=0.0,
         )
     else:
         print("Not initializing Sentry")
