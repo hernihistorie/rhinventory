918d0e858c915b429edfce5928238c70a6bb031f
Sanqui <me@sanqui.net>
2025-10-29 19:37:41 +0100

    Update deploy script

--- ops/deploy.sh
+++ ops/deploy.sh
@@ -3,7 +3,7 @@ echo "retroherna.org: "
 ssh -t retroherna.org "\
     cd /var/www/rhinventory && \
     sudo -u flask git pull && \
-    sudo -u flask poetry install && \
+    sudo -u flask /home/flask/.local/bin/uv sync && \
     sudo systemctl restart www_rhinventory && \
     sudo systemctl restart www_rhinventory_api \
 "
