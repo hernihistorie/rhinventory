9f3daecff242d9232c23876560e764ff807273f3
Sanqui <me@sanqui.net>
2025-11-20 18:43:52 +0100

    Fix migration

--- ops/deploy_with_migration.sh
+++ ops/deploy_with_migration.sh
@@ -4,8 +4,8 @@ echo "retroherna.org: "
 ssh -t retroherna.org "\
     cd /var/www/rhinventory && \
     sudo -u flask git pull && \
-    sudo -u flask poetry install && \
-    sudo -u flask poetry run alembic upgrade head && \
+    sudo -u flask /home/flask/.local/bin/uv sync && \
+    sudo -u flask /home/flask/.local/bin/uv run alembic upgrade head && \
     sudo systemctl restart www_rhinventory && \
     sudo systemctl restart www_rhinventory_api \
 "
