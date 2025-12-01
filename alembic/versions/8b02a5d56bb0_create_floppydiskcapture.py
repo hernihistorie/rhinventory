9dd84507b837b7f466e858c67be9db1798d0179c
Sanqui <me@sanqui.net>
2025-12-01 20:49:05 +0100

    We'll need to pick a different strategy for rebuilding aggregates after migrations

--- alembic/versions/8b02a5d56bb0_create_floppydiskcapture.py
+++ alembic/versions/8b02a5d56bb0_create_floppydiskcapture.py
@@ -43,14 +43,13 @@ def upgrade():
     op.get_bind().commit()
 
     # Rebuild FloppyDiskCapture aggregates from events
-    from rhinventory.app import create_app
-    from rhinventory.models.aggregates.floppy_disk_capture import FloppyDiskCapture
-    from rhinventory.event_store.event_store import EventStore
-
-    app = create_app()
-    with app.app_context():
-        event_store = EventStore()
-        event_store.rebuild_aggregates(aggregate_class=FloppyDiskCapture)
+    # from rhinventory.app import create_app
+    # from rhinventory.models.aggregates.floppy_disk_capture import FloppyDiskCapture
+    # from rhinventory.event_store.event_store import EventStore
+    # app = create_app()
+    # with app.app_context():
+    #     event_store = EventStore()
+    #     event_store.rebuild_aggregates(aggregate_class=FloppyDiskCapture)
 
 
 def downgrade():
