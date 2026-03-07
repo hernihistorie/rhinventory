80616ceabacf35a6217abbc597ef1002a89a5c13
Sanqui <me@sanqui.net>
2026-03-07 09:58:30 +0100

    MagDB: Add JSON data endpoints to improve client performance

--- rhinventory/public_blueprints/magdb.py
+++ rhinventory/public_blueprints/magdb.py
@@ -1,8 +1,10 @@
 from collections import defaultdict
-from typing import OrderedDict
+import json
+from typing import Any, OrderedDict
 
 import flask.templating
 from flask import Blueprint, redirect, render_template, request
+import yaml
 
 from rhinventory.models.magdb import Magazine, MagazineIssue, MagazineIssueVersion, IssueStatus, \
     MagazineIssueVersionFiles, MagDBFileType
@@ -16,6 +18,18 @@ def index():
     # return render_template("magdb/index.html")
     return redirect('https://herniarchiv.cz/magazines/', 308)
 
+def is_data_endpoint():
+    return request.path.endswith('.json') or request.path.endswith('.yaml')
+
+def render_yaml_data_template(template_name: str, context: dict[str, Any]):
+    data_yaml = render_template(template_name, **context)
+    if request.path.endswith('.yaml'):
+        return data_yaml, 200, {'Content-Type': 'text/yaml'}
+    elif request.path.endswith('.json'):
+        data_json = json.dumps(yaml.load(data_yaml, Loader=yaml.FullLoader))
+        return data_json, 200, {'Content-Type': 'application/json'}
+
+@magdb_bp.route("/catalog.json")
 @magdb_bp.route("/catalog.yaml")
 @magdb_bp.route("/catalog")
 def catalog():
@@ -31,14 +45,16 @@ def catalog():
 
         context["logos"][local_magazine_id].add(logo.file)
 
-    if request.path.endswith('.yaml'):
-        return render_template("magdb/catalog.yaml.jinja2", **context), 200, {'Content-Type': 'text/yaml'}
+    if is_data_endpoint():
+        return render_yaml_data_template("magdb/catalog.yaml.jinja2", context)
     # return render_template("magdb/catalog.html", **context)
     return redirect('https://herniarchiv.cz/magazines/catalog/', 308)
 
 
+@magdb_bp.route("/catalog/magazine-detail/<int:magazine_id>.json")
 @magdb_bp.route("/catalog/magazine-detail/<int:magazine_id>.yaml")
 @magdb_bp.route("/catalog/magazine-detail/<int:magazine_id>")
+@magdb_bp.route("/catalog/magazine-detail/<string:magazine_slug>.json")
 @magdb_bp.route("/catalog/magazine-detail/<string:magazine_slug>.yaml")
 @magdb_bp.route("/catalog/magazine-detail/<string:magazine_slug>")
 def magazine_detail(magazine_id: int | None = None, magazine_slug: str | None = None):
@@ -93,12 +109,13 @@ def magazine_detail(magazine_id: int | None = None, magazine_slug: str | None = 
         context["files"]["logos"][magazine.id].add(logo.file)
 
 
-    if request.path.endswith('.yaml'):
-        return render_template("magdb/magazine_detail.yaml.jinja2", **context), 200, {'Content-Type': 'text/yaml'}
+    if is_data_endpoint():
+        return render_yaml_data_template("magdb/magazine_detail.yaml.jinja2", context)
     # return render_template("magdb/magazine_detail.html", **context)
     return redirect(f'https://herniarchiv.cz/magazines/catalog/{magazine.slug or magazine.id}', 308)
 
 
+@magdb_bp.route("/miss-list.json")
 @magdb_bp.route("/miss-list.yaml")
 @magdb_bp.route("/miss-list")
 def miss_list():
@@ -137,7 +154,7 @@ def miss_list():
         )
     )
 
-    if request.path.endswith('.yaml'):
-        return render_template("magdb/miss-list.yaml.jinja2", **context), 200, {'Content-Type': 'text/yaml'}
+    if is_data_endpoint():
+        return render_yaml_data_template("magdb/miss-list.yaml.jinja2", context)
     # return render_template("magdb/miss-list.html", **context)
     return redirect('https://herniarchiv.cz/magazines/miss-list/', 308)
